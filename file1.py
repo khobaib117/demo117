7379f06947dc4bc7ef4fe1e5ea25b0fb8d54496b
khobaib117 <93250580+khobaib117@users.noreply.github.com>
2022-07-25 12:54:23 +0500

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,21 @@
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+from time import localtime
+
+activities = {8: 'Sleeping',
+              9: 'Commuting',
+              17: 'Working',
+              18: 'Commuting',
+              20: 'Eating',
+              22: 'Resting' }
+
+time_now = localtime()
+hour = time_now.tm_hour
+
+for activity_time in sorted(activities.keys()):
+    if hour < activity_time:
+        print (activities[activity_time])
+        break
+else:
+    print ('Unknown, AFK or sleeping!')
